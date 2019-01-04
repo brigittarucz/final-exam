b6bd3a6108090fa3b19177e72a6059500334a88a
Brigitta Rucz <me@example.com>
2019-01-04 10:29:12 +0100

    80% done

---
+++ javascript.js
@@ -0,0 +1,50 @@
+function burgerMenu() {
+    var navDoc = document.getElementById("navigation");
+    var overlay = document.getElementById("overlay");
+    navDoc.style.display = "block";
+    overlay.style.display = "block";
+}
+
+function closeBurgerMenu() {
+    var navDoc = document.getElementById("navigation");
+    var overlay = document.getElementById("overlay");
+    navDoc.style.display = "none";
+    overlay.style.display = "none";
+}
+
+function redirect1() {
+    window.location = "portfolioWork/work1.html";
+}
+
+function redirect2() {
+    window.location = "portfolioWork/work2.html";
+}
+
+function redirect3() {
+    window.location = "portfolioWork/work3.html";
+}
+
+function redirect4() {
+    window.location = "http://ingeniousneuron.com/animationHalloween/";
+}
+
+function redirect5() {
+    window.location = "portfolioWork/work5.html";
+}
+
+function redirect6() {
+    window.location = "http://ingeniousneuron.com/";
+}
+
+function redirect7() {
+    window.location = "portfolioWork/work7.html";
+}
+
+
+function redirect8() {
+    window.location = "portfolioWork/work8.html";
+}
+
+function redirectFc() {
+    window.location = "https://drive.google.com/open?id=1kh9G-nWJehKOfS5KaSNEGzgGVYZYxObZ";
+}
